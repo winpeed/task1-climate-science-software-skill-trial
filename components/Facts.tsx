ebec6401cd0cc5b758e2626c2eb40b72c3a1c9cf
Praise Obende <davidobende@gmail.com>
2022-03-04 12:57:36 +0100

    Remove unused useState and useEffect from code

--- components/Facts.tsx
+++ components/Facts.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { ReactElement } from "react";
 import { facts } from "../utils/facts";
 
--- containers/GuideContainer.tsx
+++ containers/GuideContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { ReactElement } from "react";
 import Facts from "../components/Facts";
 import Guide from "../components/Guide";
